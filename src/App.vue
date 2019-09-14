5001146556b11e47ac5c5d9b147f5785cd267da5
Josh Newlin <josh.newlin@clearobject.com>
2019-09-14 00:17:08 -0400

    Added firebase to the project and have a simple create user example

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,11 @@
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-spacer />
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
@@ -25,12 +18,9 @@
 import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
+	name: 'App',
+	components: {
+		HelloWorld,
+	},
 };
 </script>
